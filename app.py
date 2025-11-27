4fda2b72af7ac69f3af470a5ab983573525b4c10
Atul sehwag <s3212716@gmail.com>
2025-11-27 21:26:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 st.set_page_config(page_title="BharatNaukri AI - Sarkari Naukri Predictor 2026")
-st.markdown('<meta name="google-site-verification" content="QY_X-j0Tbesq1euGNuzayj6SPAn-_XjhHnefOPybRe8" />', unsafe_allow_html=True)
+st.markdown('<meta name="google-site-verification" content="QY_X-j0Tbesq1euGNuzayj6SPAn-_XjhHnefOPybRe8" />', unsafe_allow_html=True) #updated v2
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import LabelEncoder
@@ -71,6 +71,7 @@ st.balloons()
 st.markdown("---")
 st.markdown("**Made with ❤️ & 🇮🇳 by Atul Sehwag**")
 st.markdown("Instagram: @official_atul_sehwag001 | Share karo sabko!")
+
 
 
 
